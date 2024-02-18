5abbfc94a6a656b684cbfa9eed88e7ba60e851c5
Ron Wilder <ronwilder2000@yahoo.com>
2024-02-17 22:32:13 -0800

    Fixed for missing readings

--- read_csv.py
+++ read_csv.py
@@ -15,6 +15,7 @@ def get_bp(i,j):   # returns string aaa/aaa
     #print(i,j)
     return sysdia(lines[i][j])
 def sysdia(sdp):   # returns [alpha,alpha]
+    if sdp in ['','out']: return 0
     z=sdp.split('-')[0].split('/')
     return [int(z[0]),int(z[1])]
     #return z
@@ -27,9 +28,11 @@ def bp_avg(j):
     s = 0 # sys total
     d = 0 # dia total
     for i in range (nlines-1):
-        s = s + bp[i+1,j][0]
-        d = d + bp[i+1,j][1]
-        n = n + 1
+        v = bp.get((i,j),0)
+        if v:
+            s = s + v[0]
+            d = d + v[1]
+            n = n + 1
     return [round(s/n),round(d/n)]
 
 
@@ -39,7 +42,7 @@ def bp_avg(j):
 if __name__ == '__main__':
 
     # lines[_] is list of alpha strings
-    with open("C:/Users/ronwi/Downloads/data.csv", newline='') as sheet:
+    with open(r"C:\Users\ronwi\Downloads\__Daily Numbers - data.csv", newline='') as sheet:
         reader = csv.reader(sheet)
         lines = [line for line in reader]
     nlines = len(lines)
@@ -62,7 +65,9 @@ if __name__ == '__main__':
     bp = {}
     for i in range (nlines-1):
         for j in [1,2,3,4,5]:
-            bp[i+1,j] = get_bp(i+1,j+1)
+            #bp[i+1,j] = get_bp(i+1,j+1)
+            x = get_bp(i+1,j+1)
+            if x: bp[i+1,j] = x
     pass # for debug log
 
 
